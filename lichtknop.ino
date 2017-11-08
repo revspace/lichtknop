02afe3343d0bcf29ce912a39a92a3146068a6b32
Peetz0r <github@haas-en-berg.nl>
2017-11-08 02:22:53 +0100

    Second version, still not great

--- lichtknop.ino
+++ lichtknop.ino
@@ -6,22 +6,24 @@ const int knopje_pin = 3;
 const int lichtstate_pin = 4;
 
 void setup() {
-  pinMode(spacestate_pin, INPUT);
+  pinMode(spacestate_pin, INPUT_PULLUP);
   pinMode(knopje_pin, INPUT_PULLUP);
   pinMode(lichtstate_pin, OUTPUT);
   spacestate = digitalRead(spacestate_pin);
   lichtstate = spacestate;
+  digitalWrite(lichtstate_pin, lichtstate);
 }
 
 void loop() {
-  if(digitalRead(spacestate_pin) != spacestate) {
+  if(!digitalRead(spacestate_pin) != spacestate) {
     spacestate = !spacestate;
     lichtstate = spacestate;
+    digitalWrite(lichtstate_pin, lichtstate);
   }
 
   if(digitalRead(knopje_pin) == LOW) {
     lichtstate = !lichtstate; 
+    digitalWrite(lichtstate_pin, lichtstate);
     delay(1000);
   }
-
 }
