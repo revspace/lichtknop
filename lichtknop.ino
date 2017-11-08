6174bf7493581af770c0468868d1e1b8267ed481
Peetz0r <github@haas-en-berg.nl>
2017-11-08 03:19:50 +0100

    weird issue just got fixed. maybe. I think.

--- lichtknop.ino
+++ lichtknop.ino
@@ -30,14 +30,18 @@ const int lichtstate_pin = 4;
 const int led_pin = 13;
 
 void ignore_input (unsigned long ms) {
-  signed long target = millis() + ms;
-  while ((long) millis() - target >= 0) digitalWrite(led_pin, millis() % 200 < 100);
+  unsigned long start = millis();
+  while ((long) millis() - start < ms) {
+    digitalWrite(led_pin, millis() % 200 < 100);
+  }
 }
 
 bool read_knopje() {
-  unsigned long target = millis() + 20;
-  while ((long) millis() - target >= 0) {
-    if (digitalRead(knopje_pin)) return false;
+  unsigned long start = millis();
+  while ((long) millis() - start < 20) {
+    if (digitalRead(knopje_pin)) {
+      return false;
+    }
   }
   return true;
 }
