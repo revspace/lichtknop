817464406cb07c7b00c0020b5083dd5b9ea9bced
Peetz0r <peter@haas-en-berg.nl>
2022-09-14 11:05:24 +0200

    Generic lightstate code for other rooms

--- docs/README.md
+++ docs/README.md
@@ -2,7 +2,7 @@
 
 This projects contains code (and somehow also documentation) for the light ~~switches~~ switching logic at RevSpace.
 
-The lights should:
+The lights in the main room (*hoofdruimte*) should:
 * Switch on when the space opens
 * Switch off when the space closes
 * Toggle their state when one of the push buttons is pushed
@@ -10,12 +10,27 @@ The lights should:
 * Switch off for 25 seconds when the nomz button is pushed
 * Tell us their state so we can calculate power usage elsewhere
 
+The lights in the other rooms should:
+* Tell us their state so we can calculate power usage elsewhere
+
 ## `arduino`
 
 This code runs on the two arduinos inside the two *main switch thingies*. It listens to spacestate and the push buttons, and switches a solid state relay which then switches a contactor (Schneider Electric LC1D09) which then switches the lights.
 
-## `esp8266`
+## `esp8266-hoofdruimte`
 
 This code runs on the nodemcuv2 clone in the *auxiliary switch thingie*. It listens for MQTT topics (currently only `revspace/button/nomz`) and turns off the lights by interrupting the spacestate signal. It also listens for the helper contacts on the contactor to report the state on MQTT.
+
+## `esp8266-other`
+
+This code runs on d1-mini clones elsewhere in the space, connected after (motion activated) light switches, and their only job is to either tell us their light is on, or not tell us anything and let us know via MQTT LWT that their light is off.
+
+Building this requires you to specify the room name like this:
+
+```
+PLATFORMIO_BUILD_FLAGS='-DROOM=\"exampleroom\"' pio run -t upload
+```
+
+This will then identify as `lightspace-exampleroom` and publish to `revspace/lightstate/exampleroom`.
 
 ![Photo of the switch thingies](./photo.jpg)
---
+++ esp8266-other/src/main.ino
@@ -0,0 +1,78 @@
+/*
+    It's finally here!
+    Edit by f0x, original code from Fastled, Sebastius, Juerd
+*/
+
+#include <EEPROM.h>
+
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+#include <WiFiClient.h>
+
+#include <PubSubClient.h> // https://github.com/knolleary/pubsubclient/releases/tag/2.4
+
+boolean reconnect();
+
+// WiFi settings
+char ssid[] = "revspace-pub-2.4ghz";    //    your network SSID (name)
+char pass[] = "";             // your network password
+
+// MQTT Server settings and preparations
+const char* mqtt_server = "mosquitto.space.revspace.nl";
+WiFiClient espClient;
+
+PubSubClient client(mqtt_server, 1883, NULL, espClient);
+long lastReconnectAttempt = 0;
+
+void setup() {
+    WiFi.mode(WIFI_STA);
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println(ESP.getChipId());
+
+
+    Serial.print("Connecting to ");
+    Serial.print(ssid);
+    WiFi.begin(ssid, pass);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(50);
+        Serial.print(".");
+    }
+    Serial.println("");
+
+    Serial.print("WiFi connected");
+
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    Serial.print("MQTT name: ");
+    Serial.println("lightstate-" ROOM);
+}
+
+boolean reconnect() {
+    if (client.connect("lightstate-" ROOM, NULL, NULL, "revspace/lightstate/" ROOM, 0, false, "off")) {
+        client.publish("revspace/lightstate/" ROOM, "on");
+        client.loop();
+    }
+    return client.connected();
+}
+
+
+void loop() {
+    if (!client.connected()) {
+        long verstreken = millis();
+        if (verstreken - lastReconnectAttempt > 5000) {
+            lastReconnectAttempt = verstreken;
+            // Attempt to reconnect
+            if (reconnect()) {
+                lastReconnectAttempt = 0;
+            }
+        }
+    } else {
+        // Client connected
+        client.loop();
+
+    }
+
+}
